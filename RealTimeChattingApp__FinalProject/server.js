9e4f95962b284efe1fc054f95e8527cedde9b9df
eons-shubham <73813004+eons-shubham@users.noreply.github.com>
2021-06-15 20:44:21 +0530

    Add files via upload

---
+++ RealTimeChattingApp__FinalProject/server.js
@@ -0,0 +1,54 @@
+const express = require('express');  
+const app = express();  
+const server = require('http').createServer(app);  
+const io = require('socket.io')(server);
+
+let users = {
+    'shub': 'shub',
+}
+
+let socketMap = {}
+
+io.on('connection', (socket) => {
+    console.log('connected with socket id = ', socket.id)
+
+    function login(s, u){
+        s.join(u)
+        s.emit('logged_in')
+        socketMap[s.id]= u
+        console.log(socketMap)
+    }
+
+    socket.on('login', (data) => {
+        if(users[data.username]){
+            if(users[data.username] == data.password){
+                login(socket, data.username)
+            }
+            else{
+                socket.emit('login_failed')
+            }
+        } else{
+            users[data.username] = data.password
+            login(socket, data.username)
+        }
+        console.log(users)
+    })
+
+    socket.on('msg_send', (data) => {
+        data.from = socketMap[socket.id]
+        if(data.to){
+            io.to(data.to).emit('msg_rcvd', data)
+        } else{
+            socket.broadcast.emit('msg_rcvd', data)
+        }
+    })
+})
+
+
+
+app.use('/', express.static(__dirname + '/public'));  
+
+
+server.listen(3344, () => {
+    console.log('server running at http://localhost:3344')
+});
